83a4a1658123c590c590315b16b9587b24b0a3e9
Big-Plato <luisoctavius.sc@gmail.com>
2025-03-20 14:00:46 -0300

    test: caesarCipher function

--- index.test.js
+++ index.test.js
@@ -1,30 +1,38 @@
-import { calculator, capitalize, reverseString } from "./index.js";
+import {
+  caesarCipher,
+  calculator,
+  capitalize,
+  reverseString,
+} from "./index.js";
 
 test('write "cintia" to be "Cintia"', () => {
-    expect(capitalize('cintia')).toBe('Cintia');
+  expect(capitalize("cintia")).toBe("Cintia");
 });
 
 test('write "katasymbebekos" to "Katasymbebekos"', () => {
-    expect(capitalize('katasymbebekos')).toBe('Katasymbebekos');
+  expect(capitalize("katasymbebekos")).toBe("Katasymbebekos");
 });
 
 test('write "cintia" to be "aitnic"', () => {
-    expect(reverseString('cintia')).toBe('aitnic');
+  expect(reverseString("cintia")).toBe("aitnic");
 });
 
-test('adds 1 + 2 equal 3', () => {
-    expect(calculator.add(1, 2)).toBe(3);
+test("adds 1 + 2 equal 3", () => {
+  expect(calculator.add(1, 2)).toBe(3);
 });
 
-test('subtracts 30 - 12 equal 18', () => {
-    expect(calculator.subtract(30, 12)).toBe(18);
+test("subtracts 30 - 12 equal 18", () => {
+  expect(calculator.subtract(30, 12)).toBe(18);
 });
 
-test('divide 30 by 2 equals to 15', () => {
-    expect(calculator.divide(30, 2)).toBe(15);
+test("divide 30 by 2 equals to 15", () => {
+  expect(calculator.divide(30, 2)).toBe(15);
 });
 
-test('multiply 10 * 100 equals to 1000', () => {
-    expect(calculator.multiply(10, 100)).toBe(1000);
+test("multiply 10 * 100 equals to 1000", () => {
+  expect(calculator.multiply(10, 100)).toBe(1000);
 });
 
+test('xyz with shift factor 3 equal to "abc"', () => {
+  expect(caesarCipher("xyz", 3)).toBe("abc");
+});
